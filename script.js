ef1798897b97b2d861e607bc4bce933c011c2d3d
CarsonH19 <questline.carson@gmail.com>
2023-09-18 00:12:30 +0900

    final edits

--- script.js
+++ script.js
@@ -1,106 +1,182 @@
 // Tic-Tac-Toe 
 
-
-
-
-// Module Pattern for the gameboard
+// Define Gameboard Module
 const Gameboard = (() => {
-    const board = [
-        ['', '', ''],
-        ['', '', ''],
-        ['', '', '']
-      ];
+  let board = [];
 
-    //Public methods
-    const getBoard = () => board;
+  const resetBoard = () => {
+    board = [
+      [null, null, null],
+      [null, null, null],
+      [null, null, null]
+    ];
+  }
 
-    const addPiece = (row, col, piece) => {
-        //add logic here
+  const getBoard = () => board;
+
+  const addPiece = (row, col, piece) => {
+    if (board[row][col] === null) {
+      board[row][col] = piece;
+      return true; // Return true if piece was successfully added
+    }
+    return false; // Return false if the cell is already occupied
+  }
+
+  const checkForWinner = () => {
+    // Check rows
+    for (let i = 0; i < 3; i++) {
+      if (board[i][0] !== null && board[i][0] === board[i][1] && board[i][1] === board[i][2]) {
+        return board[i][0]; // Return the winning player symbol
+      }
     }
 
-    const clearBoard = () => {
-        //Clear board logic here
+    // Check columns
+    for (let i = 0; i < 3; i++) {
+      if (board[0][i] !== null && board[0][i] === board[1][i] && board[1][i] === board[2][i]) {
+        return board[0][i];
+      }
     }
 
-    return {
-      getBoard,
-      addPiece,
-      clearBoard
-    };
-  })();
+    // Check diagonals
+    if (board[0][0] !== null && board[0][0] === board[1][1] && board[1][1] === board[2][2]) {
+      return board[0][0];
+    }
+    if (board[0][2] !== null && board[0][2] === board[1][1] && board[1][1] === board[2][0]) {
+      return board[0][2];
+    }
 
+    // Check for a tie
+    if (!board.flat().includes(null)) {
+      return 'tie';
+    }
 
-// Factory Function for creating players
+    return null; // Return null if the game is not over yet
+  }
+
+  return {
+    resetBoard,
+    getBoard,
+    addPiece,
+    checkForWinner,
+  };
+})();
+
+// Player Function Factory
 const Player = (name, symbol) => {
-    const getName = () => name;
-    const getSymbol = () => symbol;
+  const getName = () => name;
+  const getSymbol = () => symbol;
+  const setName = (newName) => { playerName = newName };
 
-    const makeMove = (row,col) => {
-        //add logic here
-    }
-
-    return {
-        getName,
-        getSymbol,
-        makeMove
-    };
+  return {
+    getName,
+    getSymbol,
+    setName,
+  };
 };
 
+// Game Control Module Pattern
+const GameControl = (() => {
+  let player1, player2, currentPlayer;
+  let gameOver = false;
 
-// Module Pattern for controlling the flow of the game
-  function playGame ((){
-    const player1 = Player('Player 1', 'X')
-    const player2 = Player('PLayer 2', 'O')
-    let currentPlayer = player1;
+  const setPlayers = (name1, name2) => {
+    player1 = Player(name1, 'X');
+    player2 = Player(name2, 'O');
+    currentPlayer = player1;
+  }
 
-    const switchPlayer = () => {
-        currentPlayer = currentPlayer === player1 ? player2: player1;
+  const switchPlayer = () => {
+    currentPlayer = currentPlayer === player1 ? player2 : player1;
+  }
+
+  const makeMove = (row, col) => {
+    if (!gameOver) {
+      const currentPlayerSymbol = currentPlayer.getSymbol();
+
+      if (Gameboard.addPiece(row, col, currentPlayerSymbol)) {
+        const winner = Gameboard.checkForWinner();
+        if (winner) {
+          gameOver = true;
+          displayWinner(winner);
+        } else {
+          switchPlayer();
+        }
+      }
     }
+  }
 
-    const makeMove = (row, col) => {
-        //add logic here
-        switchPlayer();
-    }
+  const restartGame = () => {
+    gameOver = false;
+    Gameboard.resetBoard();
+    displayBoard();
+  }
 
-    return {
-        makeMove,
-    };
-  })();
-
-
-  const renderBoard = () => {
-    const boardElement = document.getElementById('board');
-    const board = Gameboard.getBoard();
-
-    boardElement.innerHTML = ''; // Clear previous content
-
-    board.forEach((row, i) => {
-      row.forEach((cell, j) => {
-        const cellElement = document.createElement('div');
-        cellElement.classList.add('cell');
-        cellElement.dataset.row = i;
-        cellElement.dataset.col = j;
-        cellElement.textContent = cell || ''; // Show symbol or empty string
-        cellElement.addEventListener('click', handleCellClick);
-
-        boardElement.appendChild(cellElement);
-      });
-    });
-  };
-
+  const displayWinner = (winner) => {
+    const displayElement = document.getElementsByClassName('display')[0]; 
+    const playAgainButton = document.getElementById('play-again');
   
-
-  const handleCellClick = (event) => {
-    const row = parseInt(event.target.dataset.row);
-    const col = parseInt(event.target.dataset.col);
-
-    // Check if the cell is empty before making a move
-    if (Gameboard.addPiece(row, col, GameControl.getCurrentPlayer().getSymbol())) {
-      renderBoard(); // Update the board visually
-
-      // Optionally, check for game over conditions or switch players here
+    if (winner === 'tie') {
+      displayElement.textContent = "It's a tie!";
+    } else {
+      const winningPlayer = winner === player1.getSymbol() ? player1 : player2; 
+      displayElement.textContent = `${winningPlayer.getName()} wins!`; 
     }
-  };
+  
+    playAgainButton.style.display = 'block'; 
+  }
 
-  // Start the game
-  renderBoard();
+  return {
+    setPlayers,
+    makeMove,
+    restartGame,
+  };
+})();
+
+// Display Gameboard on Webpage using DOM manipulation
+const displayBoard = () => {
+  const boardElement = document.getElementById('board');
+  boardElement.innerHTML = '';
+
+  Gameboard.getBoard().forEach((row, rowIndex) => {
+    const rowElement = document.createElement('div');
+    rowElement.classList.add('row');
+
+    row.forEach((cell, colIndex) => {
+      const cellElement = document.createElement('div');
+      cellElement.classList.add('cell');
+      cellElement.dataset.row = rowIndex;
+      cellElement.dataset.col = colIndex;
+      cellElement.textContent = cell || '';
+
+      cellElement.addEventListener('click', () => {
+        GameControl.makeMove(rowIndex, colIndex);
+        cellElement.textContent = Gameboard.getBoard()[rowIndex][colIndex] || '';
+      });
+
+      rowElement.appendChild(cellElement);
+    });
+
+    boardElement.appendChild(rowElement);
+  });
+};
+
+// Start the game
+const startGame = () => {
+  const player1Name = prompt('Enter Player 1 name:');
+  const player2Name = prompt('Enter Player 2 name:');
+  GameControl.setPlayers(player1Name, player2Name);
+  Gameboard.resetBoard();
+  displayBoard();
+  const displayElement = document.getElementsByClassName('display');
+  displayElement[0].textContent = '';
+}
+
+// Play again button
+document.getElementById('play-again').addEventListener('click', () => {
+    GameControl.restartGame();
+    document.getElementById('play-again').style.display = 'none'; // Hide the button again
+  });
+
+// Initialize the game
+startGame();
+
